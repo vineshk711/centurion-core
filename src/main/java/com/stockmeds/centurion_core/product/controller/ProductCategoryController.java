4eebe33658aef51bae051a51c66bcda4167fc844
Vinesh <vinesh.kumar@birdeye.com>
2025-02-09 18:28:56 +0530

    added get products and categories apis

--- src/main/java/com/stockmeds/centurion_core/product/controller/ProductCategoryController.java
+++ src/main/java/com/stockmeds/centurion_core/product/controller/ProductCategoryController.java
@@ -1,12 +1,7 @@
 package com.stockmeds.centurion_core.product.controller;
 
 import com.stockmeds.centurion_core.product.dto.ProductCategoryDTO;
-import com.stockmeds.centurion_core.product.entity.Product;
-import com.stockmeds.centurion_core.product.entity.ProductCategory;
 import com.stockmeds.centurion_core.product.service.ProductService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/stockmeds/centurion_core/product/controller/ProductController.java
+++ src/main/java/com/stockmeds/centurion_core/product/controller/ProductController.java
@@ -1,17 +1,12 @@
 package com.stockmeds.centurion_core.product.controller;
 
 import com.stockmeds.centurion_core.product.dto.ProductDTO;
-import com.stockmeds.centurion_core.product.entity.Product;
 import com.stockmeds.centurion_core.product.service.ProductService;
-import com.twilio.http.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
